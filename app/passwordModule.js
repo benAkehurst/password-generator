44ef9789469906118a4f30eb58db15511e9d88e7
mistabenjo <benakehurst@hotmail.com>
2017-03-04 19:24:23 +0200

    added option for 12 & 16 char passwords

--- app/passwordModule.js
+++ app/passwordModule.js
@@ -15,15 +15,20 @@
 
 			// calls a function that makes the shuffled array only 8 charaters
 			var eightCharPassword = eightCharGenerate(arrAllDigits);
+			var tenCharPassword = tenCharGenerate(arrAllDigits);
+			var twelveCharPassword = twelveCharGenerate(arrAllDigits);
+			var sixteenCharPassword = sixteenCharGenerate(arrAllDigits);
 
 			// sends the new 8 char password to the view
 			$scope.newEightCharPassword = eightCharPassword.newPassword;
+			$scope.newTenCharPassword = tenCharPassword.newPassword;
+			$scope.newTwelveCharPassword = twelveCharPassword.newPassword;
+			$scope.newSixteenCharPassword = sixteenCharPassword.newPassword;
 
 		}
 	});
 
 	function shuffleArray(all) {
-    	
     	for (var i = all.length - 1; i > 0; i--) {
         	var j = Math.floor(Math.random() * (i + 1));
 
@@ -31,7 +36,6 @@
         	all[i] = all[j];
        		all[j] = temp;
     	}	
-    	
     	return all;
 	}
 
@@ -39,6 +43,47 @@
 		var newEightCharPassword = eightDigit[0] + eightDigit[1] + eightDigit[2]+ eightDigit[3] + eightDigit[4]+ eightDigit[5] + eightDigit[6]+ eightDigit[7];
 
 		return {"newPassword":newEightCharPassword};
+	}
+
+	function tenCharGenerate(tenDigit){
+
+		// Makes a blank array
+		var arr = [];
+		// In a loop we put 10 random numbers into the array
+		for (var i = 0; i < 10; i++) {	
+			var num = parseInt((Math.random()*50));
+			arr.push(num);
+		}
+
+		// The new password is made up of the randomized numbers and letters and there order
+		var newTenCharPassword = tenDigit[arr[0]] + tenDigit[arr[1]] + tenDigit[arr[2]]+ tenDigit[arr[3]] + tenDigit[arr[4]]+ tenDigit[arr[5]] + tenDigit[arr[6]] + tenDigit[arr[7]] + tenDigit[arr[8]] + tenDigit[arr[9]];
+
+		// the new random password is returned in an object to the variable that called it.
+		return {"newPassword":newTenCharPassword};
+	}
+
+	function twelveCharGenerate(twelveChar){
+		var arr = [];
+		// In a loop we put 10 random numbers into the array
+		for (var i = 0; i < 12; i++) {	
+			var num = parseInt((Math.random()*50));
+			arr.push(num);
+		}
+		var newTwelveCharPassword = twelveChar[arr[0]] + twelveChar[arr[1]] + twelveChar[arr[2]] + twelveChar[arr[3]] + twelveChar[arr[4]] + twelveChar[arr[5]] + twelveChar[arr[6]] + twelveChar[arr[7]] + twelveChar[arr[8]] + twelveChar[arr[9]] + twelveChar[arr[10]] + twelveChar[arr[11]];
+
+		return {"newPassword":newTwelveCharPassword};
+	}
+
+	function sixteenCharGenerate(sixteenChar){
+		var arr = [];
+		// In a loop we put 10 random numbers into the array
+		for (var i = 0; i < 16; i++) {	
+			var num = parseInt((Math.random()*50));
+			arr.push(num);
+		}
+		var newSixteenCharPassword = sixteenChar[arr[0]] + sixteenChar[arr[1]] + sixteenChar[arr[2]] + sixteenChar[arr[3]] + sixteenChar[arr[4]] + sixteenChar[arr[5]] + sixteenChar[arr[6]] + sixteenChar[arr[7]] + sixteenChar[arr[8]] + sixteenChar[arr[9]] + sixteenChar[arr[10]] + sixteenChar[arr[11]] + sixteenChar[arr[12]] + sixteenChar[arr[13]] + sixteenChar[arr[14]] + sixteenChar[arr[15]];
+
+		return {"newPassword":newSixteenCharPassword};
 	}
 
 })();
